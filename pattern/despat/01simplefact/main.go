70a19582b6957d3cc942121a19b9b9b525a9b6ad
HsiaoCz <984274788@qq.com>
2024-05-08 09:22:55 +0800

    some

---
+++ pattern/despat/01simplefact/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+// 简单工厂模式
+// 工厂（Factory）角色：简单工厂模式的核心，它负责实现创建所有实例的内部逻辑。
+// 工厂类可以被外界直接调用，创建所需的产品对象。
+// 抽象产品（AbstractProduct）角色：简单工厂模式所创建的所有对象的父类，它负责描述所有实例所共有的公共接口。
+// 具体产品（Concrete Product）角色：简单工厂模式所创建的具体实例对象
+
+// ======= 抽象层 =========
+
+// 水果类(抽象接口)
+type Fruit interface {
+	Show() //接口的某方法
+}
+
+// ======= 基础类模块 =========
+
+type Apple struct {
+	Fruit //为了易于理解显示继承(此行可以省略)
+}
+
+func (apple *Apple) Show() {
+	fmt.Println("我是苹果")
+}
+
+type Banana struct {
+	Fruit
+}
+
+func (banana *Banana) Show() {
+	fmt.Println("我是香蕉")
+}
+
+type Pear struct {
+	Fruit
+}
+
+func (pear *Pear) Show() {
+	fmt.Println("我是梨")
+}
+
+// ========= 工厂模块  =========
+// 一个工厂， 有一个生产水果的机器，返回一个抽象水果的指针
+type Factory struct{}
+
+func (fac *Factory) CreateFruit(kind string) Fruit {
+	var fruit Fruit
+
+	if kind == "apple" {
+		fruit = new(Apple)
+	} else if kind == "banana" {
+		fruit = new(Banana)
+	} else if kind == "pear" {
+		fruit = new(Pear)
+	}
+
+	return fruit
+}
+
+// ==========业务逻辑层==============
+func main() {
+	factory := new(Factory)
+
+	apple := factory.CreateFruit("apple")
+	apple.Show()
+
+	banana := factory.CreateFruit("banana")
+	banana.Show()
+
+	pear := factory.CreateFruit("pear")
+	pear.Show()
+}
