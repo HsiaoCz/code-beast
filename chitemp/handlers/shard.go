874ba45928ab00dc360360ff182bac00df2d472b
HsiaoCz <984274788@qq.com>
2024-05-16 10:57:54 +0800

    some

--- chitemp/handlers/shard.go
+++ chitemp/handlers/shard.go
@@ -17,23 +17,29 @@ func (a APIError) Error() string {
 	return a.Msg
 }
 
+type Status struct {
+	Code int
+}
 type Handlerfunc func(w http.ResponseWriter, r *http.Request) error
 
 func TransferHandlerFunc(h Handlerfunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		status := &Status{Code: http.StatusOK}
 		if err := h(w, r); err != nil {
 			slog.Error("the http server error", "method", r.Method, "path", r.URL.Path, "remote address", r.RemoteAddr, "error message", err)
 			if e, ok := err.(APIError); ok {
+				status.Code = e.Status
 				WriteJSON(w, e.Status, &e)
 			} else {
 				arr := APIError{
 					Status: http.StatusInternalServerError,
 					Msg:    err.Error(),
 				}
+				status.Code = arr.Status
 				WriteJSON(w, arr.Status, &arr)
 			}
 		}
-		slog.Info("new request comming", "method", r.Method, "path", r.URL.Path, "remote address", r.RemoteAddr)
+		slog.Info("new request comming", "method", r.Method, "code", status.Code, "path", r.URL.Path, "remote address", r.RemoteAddr)
 	}
 }
 
--- chitemp/handlers/user_handler.go
+++ chitemp/handlers/user_handler.go
@@ -1,14 +1,19 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	views "github.com/HsiaoCz/code-beast/chitemp/views/layout"
+	"github.com/HsiaoCz/code-beast/chitemp/views/uservs"
+)
 
 type UserHandler struct {
 }
 
-func NewUserHandler()*UserHandler{
+func NewUserHandler() *UserHandler {
 	return &UserHandler{}
 }
 
 func (u *UserHandler) HandleShow(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	return Render(w, r, views.Base(uservs.Show()))
 }
