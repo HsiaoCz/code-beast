288f857a82eb03ee875a3c927c84fabeb12986fc
HsiaoCz <984274788@qq.com>
2024-08-06 00:23:39 +0800

    add some

---
+++ crazy/actor/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Handler struct {
+	lock  sync.Mutex
+	state uint
+}
+
+func (h *Handler) handleMessage(msg uint) {
+	h.lock.Lock()
+	defer h.lock.Unlock()
+	h.state = msg
+}
+
+func main() {
+	h := &Handler{}
+	for i := 0; i < 10; i++ {
+		h.handleMessage(uint(i))
+		fmt.Println(h.state)
+	}
+}
