63a4872fbbe3cdacca18d820da62480ae37c8a6b
HsiaoCz <984274788@qq.com>
2024-09-04 06:59:46 +0800

    add some

---
+++ andy/chat/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWsOrderBook(ws *websocket.Conn) {
+	logrus.WithFields(logrus.Fields{
+		"remote address": ws.RemoteAddr(),
+	}).Info("new incoming connection from client to order book feed")
+	for {
+		paylod := fmt.Sprintf("orderbook data -> data %d\n", time.Now().UnixNano())
+		ws.Write([]byte(paylod))
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	logrus.WithFields(logrus.Fields{
+		"remote address": ws.RemoteAddr(),
+	}).Info("new incoming connection from client")
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.Handle("/orderbookfeed", websocket.Handler(server.handleWsOrderBook))
+	http.ListenAndServe(":9001", nil)
+}
