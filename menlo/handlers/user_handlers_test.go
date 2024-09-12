a3fad38a0d7d36f89e049608f9bdd2db82a27f02
HsiaoCz <984274788@qq.com>
2024-09-13 00:10:01 +0800

    add some

---
+++ menlo/handlers/user_handlers_test.go
@@ -0,0 +1,5 @@
+package handlers
+
+import "testing"
+
+func TestCreateUser(t *testing.T) {}
--- menlo/main.go
+++ menlo/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/HsiaoCz/code-beast/menlo/db"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("all is well")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.InitDB(); err != nil {
+		log.Fatal(err)
+	}
+
+	var (
+		addr   = os.Getenv("PORT")
+		router = http.NewServeMux()
+	)
+
+	http.ListenAndServe(addr,router)
 }
