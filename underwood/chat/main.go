284e58ee2f8457fdf64927e5ce91beb712ea07e8
HsiaoCz <984274788@qq.com>
2024-09-18 17:13:08 +0800

    add some

---
+++ underwood/chat/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+
+func (s *Server) handleWsOrderBook(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client to order book feed:", ws.RemoteAddr())
+	for {
+		paylod := fmt.Sprintf("orderbook data ---> %d\n", time.Now().UnixNano())
+		ws.Write([]byte(paylod))
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error:", err)
+			continue
+		}
+		msg := buf[:n]
+		s.broadcast(msg)
+	}
+}
+
+func (s *Server) broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error:", err)
+			}
+		}(ws)
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.Handle("/orderbookfeed", websocket.Handler(server.handleWsOrderBook))
+	http.ListenAndServe(":9001", nil)
+}
---
+++ underwood/gingg/types.go
@@ -0,0 +1,6 @@
+package main
+
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
